885a4e74dd7739e222806fd8f55f46f62cda3a95
claytoncook12 <claytoncook12@gmail.com>
2022-04-04 13:40:06 -0400

    init

---
+++ static/js/print.js
@@ -0,0 +1,11 @@
+// function to print area with id=printablearea
+function printDiv(divName) {
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+}
